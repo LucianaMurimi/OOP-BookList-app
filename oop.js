a09ab7aad502c99b5b4bd1b73ab5a4d8f14ad1de
technical <technical@silicon-savannah.com>
2020-11-16 19:42:34 +0300

    autocomplete search

--- oop.js
+++ oop.js
@@ -25,11 +25,33 @@ class UI {
             let li = document.createElement('li');
             li.setAttribute("id", `${books[i].id}`);
             li.innerHTML = (`
-                ${books[i].title} by ${books[i].author}
+                ${books[i].title} <span class="author">by ${books[i].author}<span>
                 <span class="delete"><i class="fa fa-trash"></i></span>
             `);
             ul.append(li);
         }        
+    }
+    static createSearchList(listItems) {
+        let searchItem = document.getElementById('searchID').value;
+
+        if(searchItem !== ''){
+            let books = Store.getBooks();
+            let searchRes = books.filter(book=>book.title.includes(searchItem) || book.author.includes(searchItem));
+            
+            let ul = document.querySelector('.search-results');
+            ul.innerHTML = ' ';
+            
+            //Loop through each element in the array creating a list item
+            for(let i in searchRes ){
+                let li = document.createElement('li');
+                li.setAttribute("id", `${searchRes[i].id}`);
+                li.innerHTML = (`
+                    ${searchRes[i].title} <span class="author">by ${searchRes[i].author}<span>
+                `);
+                ul.append(li);
+            }       
+        }
+        
     }
     static listenForDelete() {
         let listItems = document.querySelectorAll('.delete');
@@ -50,7 +72,6 @@ class UI {
             });
         }
        
-        //UI.addBookToList()
     }
 }
 
@@ -66,8 +87,84 @@ class Store {
         books.push(book);
         localStorage.setItem('books', JSON.stringify(books));
     }
-}
+    static searchList() {
+        let books = Store.getBooks();
+        let titleArr = books.map(book=>book.title);
+        let authorArr = books.map(book=>book.author);
+        let booksArr = titleArr.concat(authorArr); 
+        return booksArr;
+    }
+    static autocomplete(inp, arr){
+        /*the autocomplete function takes two arguments => 1.   the text field element
+                                                            2.  an array of possible autocompleted values
+        */
+       
+        /*execute a function when someone writes in the text field:*/
+        inp.addEventListener('input', e => {
+            let ul, liItem, val = e.target.value;
+            //console.log(e.target.value)
+            
+            /*close any already open lists of autocompleted values*/
+            closeAllLists();
 
+            if (!val) { return false;}  //if there is no value entered, exit; do not show any list
+            
+            /*create a div element that will contain the li items/values i.e
+            <div id = "searchIDautocomplete-list" class="autocomplete-items">
+            </div>
+            */
+            ul = document.createElement('div');
+            ul.setAttribute("class", "autocomplete-items");
+            e.target.parentNode.appendChild(ul);
+
+            /*for each item in the array...*/
+            for (let i = 0; i < arr.length; i++) {
+                /*check if the item starts with the same letters as the text field value:*/
+                if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                    /*create a li element for each matching element:*/
+                    liItem = document.createElement('li');
+                    liItem.setAttribute('class', 'litems');
+
+                    /*make the matching letters bold:*/
+                    liItem.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                    liItem.innerHTML += arr[i].substr(val.length);   /*concatenate it with the rest of the string*/
+                    //console.log(arr[i]);
+                    
+                    /*insert a input field that will hold the current array item's value:*/
+                    liItem.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                    
+                    liItem.addEventListener("click", e=>{
+                    /*insert the value for the autocomplete text field:*/
+                        inp.value = e.target.innerText;
+                        //then, close the list of autocompleted values
+                        closeAllLists();
+                    });
+                    ul.appendChild(liItem);
+                }
+            }
+        });
+        function closeAllLists(){
+            //close all autocomplete lists in the document,
+            let x = document.getElementsByClassName("autocomplete-items");
+            
+            for (let i = 0; i < x.length; i++) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+        /*also, close all lists when someone clicks anywhere elese on the document:*/
+        document.addEventListener("click", function (e) {
+            closeAllLists(e.target);
+        });
+
+    }
+    /*static getSearchItems(item) {
+        let books = Store.getBooks();
+        let searchRes = books.filter(book=>book.title.includes(item) || book.author.includes(item));
+        UI.createSearchList(searchRes);
+        
+    }*/
+    
+}
 
 //Listen for an add event
 document.querySelector('.add').addEventListener('click', (e)=>{
@@ -87,10 +184,15 @@ document.querySelector('.add').addEventListener('click', (e)=>{
 });
 
 
-//Load the booklist on loading content
+/*on document load: 1.  create the booklist
+                    2.  listen for delete events
+                    3.  listen for search events*/
 document.addEventListener('DOMContentLoaded', ()=>{
     UI.addBookToList();
     UI.listenForDelete();
+    //Store.autocomplete(theInput, theArray)
+    Store.autocomplete(document.getElementById('searchID'), Store.searchList());
+    UI.createSearchList(document.getElementById('searchID').value);
 });
 
 
